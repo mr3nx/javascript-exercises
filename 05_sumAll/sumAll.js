d6f99f5e63ac90ece6dc6e90b3a27eada21ecc2e
mr3nx <astonishingluck@gmail.com>
2024-01-26 00:58:43 +1100

    Passed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!(Number.isInteger(start) && Number.isInteger(end))) {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (start > end) [start, end] = [end, start];
+    for (let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
