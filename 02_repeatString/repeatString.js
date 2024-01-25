fe43e9d28543212f0aa73fe7ad06de4db01b2621
mr3nx <astonishingluck@gmail.com>
2024-01-26 00:58:43 +1100

    Passed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newStr;
+    for(let i = 0; i < num; i++){
+        newStr += str;
+    }
+    return newStr;
 
 };
 
